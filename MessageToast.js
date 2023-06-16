6991e7285ee4e95c36bcde65dc42d3b6fc094eaf
Leonidas Goetz <leonidas.goetz@sap.com>
2023-06-16 19:17:28 -0300

    chore: remove unused toasts

--- MessageToast.js
+++ MessageToast.js
@@ -1,32 +1,16 @@
 
 const SUCCESS_TOAST_CLASS = "rounded-1 me-2 p-2 bg-success";
 const ERROR_TOAST_CLASS = "rounded-1 me-2 p-2 bg-danger";
-const WARNING_TOAST_CLASS = "rounded-1 me-2 p-2 bg-warning";
-const INFO_TOAST_CLASS = "rounded-1 me-2 p-2 bg-info";
 class MessageToast {
   constructor() {
     this.toastElement = document.getElementById("liveToast");
     this.toast = bootstrap.Toast.getOrCreateInstance(this.toastElement);
   }
 
-  mountInfoToast(message) {
-    document.getElementById("toastMessage").innerHTML = message;
-    document.getElementById("toastIcon").setAttribute("class", INFO_TOAST_CLASS);
-    document.getElementById("toastTitle").innerHTML = "Info";
-    return this;
-  }
-
   mountSuccessToast(message) {
     document.getElementById("toastMessage").innerHTML = message;
     document.getElementById("toastIcon").setAttribute("class", SUCCESS_TOAST_CLASS);
     document.getElementById("toastTitle").innerHTML = "Success";
-    return this;
-  }
-
-  mountWarningToast(message) {
-    document.getElementById("toastMessage").innerHTML = message;
-    document.getElementById("toastIcon").setAttribute("class", WARNING_TOAST_CLASS);
-    document.getElementById("toastTitle").innerHTML = "Warning";
     return this;
   }
 
